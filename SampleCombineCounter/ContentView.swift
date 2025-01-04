906dea26be2f0327ce69f22851474fe7fed89f70
Antwhale <olollolo339@naver.com>
2025-01-04 20:48:30 +0900

    Initial Commit

---
+++ SampleCombineCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleCombineCounter
+//
+//  Created by 부재식 on 1/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleCombineCounter/SampleCombineCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCombineCounterApp.swift
+//  SampleCombineCounter
+//
+//  Created by 부재식 on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleCombineCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
